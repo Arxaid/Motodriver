903befc76cf3bb23296756ce8fb217a2dbdb71a1
Arxaid <VallravenWasTaken@gmail.com>
2022-07-08 14:37:56 +0300

    Testing sketch

--- src/motodriver.ino
+++ src/motodriver.ino
@@ -11,15 +11,15 @@ void setup() {
   Serial.begin(9600);
 }
 
-Motor motor1 = Motor(10,8,9,1);
+Motor motor1 = Motor(10,8,9,ShieldDriversNames::L298N);
 std::map<int8_t, Motor> motorList = { {1, motor1} };
 MotoDriver motodriver = MotoDriver(motorList);
 
 void loop() {
   motodriver.Forward(255, 1);
   delay(5000);
-  //motodriver.Backward(50, 1);
-  //delay(5000);
-  //motodriver.Shutdown(1);
-  //delay(10000);
+  motodriver.Backward(50, 1);
+  delay(5000);
+  motodriver.Shutdown(1);
+  delay(10000);
 }
